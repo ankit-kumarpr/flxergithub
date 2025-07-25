fa52be8a9d03cf743cf3bf52be137769e700f953
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-07-25 18:11:00 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const sendEmail = require("../utils/sendEmail");
 const welcomeTemplate = require("../utils/templates/welcomeTemplate");
 const Membership = require("../models/membershipModel");
+const Referral = require("../models/referralModel");
 
 
 const generateCustomID = (username) => {
@@ -12,43 +13,107 @@ const generateCustomID = (username) => {
 };
 
 // Register
+// const RegisterUser = async (req, res) => {
+//   try {
+//     const { name, email, phone, password, role } = req.body;
+
+//     const existing = await User.findOne({ email });
+
+//     if (existing)
+//         {
+        
+//         return res.status(400).json({
+//             error:true,
+//          message: "User already exists"
+//          });
+//         }
+
+//     const hashed = await bcrypt.hash(password, 10);
+//     const customID = generateCustomID(name);
+
+//     const newUser = await User.create({
+//       name,
+//       email,
+//       phone,
+//       password: hashed,
+//       role,
+//       customID
+//     });
+
+//     const html = welcomeTemplate(name, customID, email, password);
+//     await sendEmail(email, "Welcome to Flexr!", html);
+
+//    return res.status(201).json({
+//         error:false,
+//          message: "Registered Successfully",
+//           newUser 
+//         });
+//   } catch (err) {
+//    return res.status(500).json({ message: err.message });
+//   }
+// };
+
+
 const RegisterUser = async (req, res) => {
   try {
     const { name, email, phone, password, role } = req.body;
+    const ref = req.query.ref; // Grab ?ref=customID from query
 
     const existing = await User.findOne({ email });
-
-    if (existing)
-        {
-        
-        return res.status(400).json({
-            error:true,
-         message: "User already exists"
-         });
-        }
+    if (existing) {
+      return res.status(400).json({
+        error: true,
+        message: "User already exists"
+      });
+    }
 
     const hashed = await bcrypt.hash(password, 10);
     const customID = generateCustomID(name);
 
-    const newUser = await User.create({
+    // Build user object
+    const userData = {
       name,
       email,
       phone,
       password: hashed,
       role,
-      customID
-    });
+      customID,
+    };
 
+    // If user came from a referral link, attach referral info
+    if (ref) {
+      const referrerUser = await User.findOne({ customID: ref });
+      if (referrerUser) {
+        userData.referralLink = ref; // Store referrer’s customID in the user document
+      }
+    }
+
+    // Create user
+    const newUser = await User.create(userData);
+
+    // If referral exists, log it in the Referral collection
+    if (ref) {
+      const referrerUser = await User.findOne({ customID: ref });
+      if (referrerUser) {
+        await Referral.create({
+          referrer: referrerUser._id,
+          referredUser: newUser._id,
+        });
+      }
+    }
+
+    // Send welcome email
     const html = welcomeTemplate(name, customID, email, password);
     await sendEmail(email, "Welcome to Flexr!", html);
 
-   return res.status(201).json({
-        error:false,
-         message: "Registered Successfully",
-          newUser 
-        });
+    return res.status(201).json({
+      error: false,
+      message: "Registered Successfully",
+      newUser,
+    });
   } catch (err) {
-   return res.status(500).json({ message: err.message });
+    console.error("Registration error:", err);
+    return res.status(500).json({ message: err.message });
   }
 };
 
