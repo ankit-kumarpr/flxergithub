a95f0583ac8ac54519ce2adb372a7d7fe38d34fc
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-07-25 10:05:31 +0530

    Update membershipRoutes.js

--- routes/membershipRoutes.js
+++ routes/membershipRoutes.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
 const { buyMembership, getMyReferrals } = require("../controllers/membershipController");
+const paymentcontrol = require("../controllers/paymentcontroller");
 
 router.post("/buy", auth, buyMembership);
 router.get("/referrals", auth, getMyReferrals);
+
+// payment routes
+router.post("/orders", paymentcontrol.orders);
+router.post("/verify", paymentcontrol.verify);
 
 module.exports = router;
